27901aa6f1b7ff0d3c9cb58e038818f914f7b57c
CALKO9611 <kimd9669@naver.com>
2023-06-21 17:53:00 +0900

    BAEKJOON PYTHON 1110

---
+++ BAEKJOON/PYTHON/BRONZE/Ⅰ/1110/1110.py
@@ -0,0 +1,15 @@
+N = int(input())
+num = N
+cycle = 0
+
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+    cycle += 1
+
+    if num == N:
+        break
+
+print(cycle)
