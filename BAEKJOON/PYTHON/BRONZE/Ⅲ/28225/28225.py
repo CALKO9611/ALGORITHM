7392c94be07e191f770513617f50e21ffc7c3e2e
CALKO9611 <kimd9669@naver.com>
2023-06-22 15:15:30 +0900

    BAEKJOON PYTHON 28225

---
+++ BAEKJOON/PYTHON/BRONZE/Ⅲ/28225/28225.py
@@ -0,0 +1,7 @@
+n, f = map(int, input().split())
+record = []
+for _ in range(n):
+    x, v = map(int, input().split())
+    record.append((f - x) / v)  # (목표 거리 - 현재 위치) / 속력
+
+print(record.index(min(record)) + 1)
