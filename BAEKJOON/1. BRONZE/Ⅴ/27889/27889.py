7f1251bddbb7b026db9e7715c698dccb813da960
CALKO9611 <kimd9669@naver.com>
2023-12-20 18:03:01 +0900

    BAEKJOON PYTHON 27889

---
+++ BAEKJOON/1. BRONZE/Ⅴ/27889/27889.py
@@ -0,0 +1,10 @@
+schools = [
+    "North London Collegiate School",
+    "Branksome Hall Asia",
+    "Korea International School",
+    "St. Johnsbury Academy",
+]
+abbreviation = ["NLCS", "BHA", "KIS", "SJA"]
+
+school = input()
+print(schools[abbreviation.index(school)])
