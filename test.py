fe819cb1b6928f558339119b466c712f0694e21e
CALKO9611 <kimd9669@naver.com>
2024-01-04 09:48:00 +0000

    prettier 설정 확인 TEST

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 print("Replit과 Github 연동 확인 TEST")
 print("username과 useremail 설정 TEST")
+print("prettier 설정 확인 TEST")
+
+# prettier 파이썬 파일 적용 방법
+# 1. 설치 (진행 완료)
+# pip install black
+# 2. 적용 (테스트 성공)
+# black [파일명]
+# 현재 이 파일 같은 경우는 black test.py 입력하면 된다.
