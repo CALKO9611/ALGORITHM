b04837fca5d986f733eff431b3837af3e2f484fb
CALKO9611 <kimd9669@naver.com>
2024-07-28 18:02:22 +0900

    BAEKJOON PYTHON 31246

---
+++ BAEKJOON/2. SILVER/5. Ⅴ/31246/31246.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+X = []
+
+for _ in range(N):
+    A, B = map(int, input().split())
+    X.append(B - A)
+
+X.sort()  # 오름차순 정렬
+
+if X[K - 1] < 0:
+    print(0)
+else:
+    print(X[K - 1])
