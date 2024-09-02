5a713711f6a1df08f7988ae3c239c6451cc79ae5
CALKO9611 <kimd9669@naver.com>
2024-09-02 23:01:31 +0900

    BAEKJOON PYTHON 32132

---
+++ BAEKJOON/1. BRONZE/1. Ⅰ/32132/32132.py
@@ -0,0 +1,10 @@
+N = int(input())
+word = input()
+
+while True:
+    if "PS4" in word or "PS5" in word:
+        word = word.replace("PS4", "PS").replace("PS5", "PS")
+    else:
+        break
+
+print(word)
