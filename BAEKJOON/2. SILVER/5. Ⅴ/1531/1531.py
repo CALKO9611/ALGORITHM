635141a6c23e6f4670b3767195f7a58e1d8ea569
CALKO9611 <kimd9669@naver.com>
2025-03-16 14:48:23 +0900

    BAEKJOON PYTHON 1531

---
+++ BAEKJOON/2. SILVER/5. Ⅴ/1531/1531.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+paper = [[0] * 101 for _ in range(101)]
+result = 0
+
+for _ in range(N):
+    x1, y1, x2, y2 = map(int, input().split())
+    for row in range(x1, x2 + 1):
+        for col in range(y1, y2 + 1):
+            paper[row][col] += 1
+
+# 보이지 않는 그림의 개수 세기
+for p in paper:
+    result += len([x for x in p if x > M])
+
+print(result)
